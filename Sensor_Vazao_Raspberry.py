066138d61aec4ab20454ba3e48e86a6845c3d794
Luciano <luciano.santos.dsr@gmail.com>
2017-10-31 14:57:37 -0200

    Projeto TCC - Gota Digital

---
+++ Sensor_Vazao_Raspberry.py
@@ -0,0 +1,32 @@
+import MySQLdb
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+while True:
+    read_serial = ser.readline()
+    print "Vazao ", read_serial
+
+    if (read_serial.find("0.00") == -1):
+
+        read_serial = read_serial.replace("\n", "")
+        db = MySQLdb.connect("localhost", "root", "senha", "instancia_db")
+
+        cursor = db.cursor()
+
+        sql = "INSERT INTO tbvazao (vazao) VALUES ('%s')" % \
+              (read_serial)
+
+        try:
+
+            cursor.execute(sql)
+
+            db.commit()
+            print "Salvo"
+
+        except:
+
+            db.rollback()
+            print "Nao Salvo"
+
+        db.close()
